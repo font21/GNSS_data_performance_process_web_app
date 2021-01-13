0b211b9d6962b0ecd6181b21f06c37819a27161f
David Fontenot <font21@gmail.com>
2021-01-13 12:21:56 -0600

    Esperimenting with radio buttons/??

--- js/app.js
+++ js/app.js
@@ -4,7 +4,17 @@
 // The Document Ready Event
 $(document).ready(function(){
 
-    console.log('jQuery connected.');
+	// Say things loaded.
+	console.log('JavaScript connected.');
+	console.log('jQuery connected.');
+});
 
+$(document).ready(function(){
+    $('input[type="radio"]').click(function(){
+        var inputValue = $(this).attr("value");
+        var targetBox = $("." + inputValue);
+        $(".box").not(targetBox).hide();
+        $(targetBox).show();
+    });
 });
 
