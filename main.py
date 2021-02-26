3291ecb1ca8a681774ef2bae40edf432d3fbda5b
David Fontenot <font21@gmail.com>
2021-02-26 14:41:44 -0600

    The phyton code is able to move sentences whose second element is GGA, GST, or VTG into another list, ggaList, gstList, and vtgList, respectivly.

--- main.py
+++ main.py
@@ -4,11 +4,29 @@ import sys, getopt
 import os
 import fileinput
 
+# Global Variables
+global inputfile
+global outputfile
+global ggaDict
+global gstDict
+global vtgDict
+
+
 def main(argv):
 	# https://www.tutorialspoint.com/python/python_command_line_arguments.htm
-	
+
 	inputfile = ''
 	outputfile = ''
+
+	ggaDict = {}
+	gstDict = {}
+	vtgDict = {}
+
+	staticLineList = []
+	ggaList = []
+	gstList = []
+	vtgList = []
+
 	try:
 		opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
 	except getopt.GetoptError:
@@ -19,32 +37,81 @@ def main(argv):
 			print 'test.py -i <inputfile> -o <outputfile>'
 			sys.exit()
 		elif opt in ("-i", "--ifile"):
-			try:
-				# https://www.guru99.com/reading-and-writing-files-in-python.html
-				inputfile = arg
-				f = open(inputfile, "r")
-				staticInputFileContents = f.read()
-				# Debug - after file contents assign
-				# print(staticInputFileContents)
-				staticLineList = staticInputFileContents.splitlines()
-				# Debug - after line split to list
-				# print(staticLineList)
-				for line in fileinput.input():
+			inputfile = arg
+			#try:
+			# https://www.guru99.com/reading-and-writing-files-in-python.html	
+			inFile = open(inputfile, "r")
+			staticInputFileContents = inFile.read()
+			# Debug - after file contents assign
+			# print(staticInputFileContents)
+			staticLineList = staticInputFileContents.splitlines()
+			# Debug - after line split to list
+			# print(staticLineList)
+			i = 0
+			while i < len(staticLineList):
+				thisLine = staticLineList[i]
+				# print(thisLine)
+				# sepSentence(staticLineList[i])
+
+				if thisLine[3:6] == 'GGA':
+					ggaList.append(thisLine)
+				elif thisLine[3:6] == 'GST':
+					gstList.append(thisLine)
+				elif thisLine[3:6] == 'VTG':
+					vtgList.append(thisLine)
+				i = i + 1
+				
+			# print(ggaList)
+			# print(gstList)
+			# print(vtgList)
+			# File In clean up
+			print('File In section complete.')
+			inFile.close()
 	
-			except:
-				print('Path ' + inputfile + ' does not exist.')
+			#except:
+			#	print('Path ' + inputfile + ' does not exist.')
+
 		elif opt in ("-o", "--ofile"):
 			outputfile = arg
-	print 'Input file is ', inputfile
-	print 'Output file is ', outputfile
+			outputfileContents = 'GGA Sentences: \n' + str(ggaList) + '\n\nGST Sentences: \n' + str(gstList) + '\n\nVTG Sentences: \n' + str(vtgList)
+			print(outputfileContents)
+			#try:
+			wOut = open(outputfile, "w")
+			# wOut.write("Hello World")
+			wOut.write(outputfileContents)
+		
+			# File Out clean up
+			print('File Out section complete.')
+			wOut.close()
+
+			#except:
+			#	print('Path ' + outputfile + ' does not exist.')
+
+	print 'Input file is',inputfile
+	print 'Output file is',outputfile
 
 # Prints elements starting from zero position till 2nd
-def talkerId(inString):
-	return inString[0:2]
+def findTalkerSent(inString):
+	firstElement = inString.pop(1)
+	talkerId = firstElement[0:2]
+	SentenceType = firstElement[3:]
+	return inString
 
-# Prints elements starting from 3rd element
-def sentenceType(inString):
-	return inString[3:]
+# Function to move sentences whose second element is GGA, GST, or VTG
+# into another list, ggaList, gstList, and vtgList, respectivly.
+def sepSentence(inList):
+	print('sepSentence() function started. Whole line is:')
+	print(inList)
+	print('inList[3:6] is:')	
+	print(inList[3:6])
+
+	if inList[3:6] == 'GGA':
+		ggaList.append(inList)
+	elif inList[3:6] == 'GST':
+		gstList.append(inList)
+	elif inList[3:6] == 'VTG':
+		vtgList.append(inList)
+	return
 
 
 def splitty(inputString):
